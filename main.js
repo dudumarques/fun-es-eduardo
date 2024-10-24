8d4b042a88ac987e3d08493a1e80da6b8606895b
Eduardo <107561143+dudumarques@users.noreply.github.com>
2024-10-24 17:23:54 -0300

    24/10

--- main.js
+++ main.js
@@ -107,7 +107,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049..."
+    caixaPerguntas.textContent = "Em 2024..."
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
